dbb7cfbdebd6c85ef2f77b7dcf557fe15ad9c9c5
Bileonaire <kiokoleon@gmail.com>
2021-12-08 12:56:21 +0300

    Dashboard project setup

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,29 @@
+import "./sidebar.css";
+import { Link } from "react-router-dom";
+
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                leon
+            </div>
+        </div>
+
+    )
+}
+
