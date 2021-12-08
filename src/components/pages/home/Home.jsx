1fce60f74d7c0f5bb31d11e04466fde2b29bef54
Bileonaire <kiokoleon@gmail.com>
2021-12-08 17:15:54 +0300

    Create Widgets

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,17 @@
-import { FeaturedInfo, Chart } from "../../index";
 import "./home.css";
+import { FeaturedInfo, Chart, WidgetLg, WidgetSm } from "../../index";
+import { userData } from "../../../dummyData";
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+
+            <div className="widgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
